e2bb57952effce40f1653e0dc6aa41cf7adfdd1b
Bryan Hunter <bdhunter3141@gmail.com>
2017-09-10 22:53:25 +0000

    Working algorithm created that passes all tests

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
-var totalBottles = function(dollars) {
+  // For every two empty bottles, you can get one free (full) bottle of pop
+  // Each bottle of pop costs $2 to purchase
+  // For every four bottle caps, you can get one free (full) bottle of pop
 
-// For every two empty bottles, you can get one free (full) bottle of pop
-// For every four bottle caps, you can get one free (full) bottle of pop
-// Each bottle of pop costs $2 to purchase
+var resultBottles = null;
+var bottles = null;
+var currentBottleCaps = [];
 
+var bottlesForDollars = function(dollars) {
+  bottles = dollars / 2;
+  return bottles;
 }
+
+var emptyToFull = function(bottles) {
+  if (bottles > 1) {
+    var newBottles = Math.floor(bottles / 2);
+    var leftoverBottles = bottles % 2;
+    currentBottleCaps.push(newBottles);
+    resultBottles += newBottles;
+    newBottles += leftoverBottles;
+    return newBottles;
+  }
+  return;
+}
+
+var bottleCapsToFull = function(bottleCaps) {
+  if (bottleCaps > 1) {
+    var newBottles = Math.floor(bottleCaps / 4);
+    var leftoverBottleCaps = bottleCaps % 4;
+    currentBottleCaps.push(leftoverBottleCaps + newBottles);
+    resultBottles += newBottles;
+    return newBottles;
+  }
+  return;
+}
+
+
+var totalBottles = function(dollars) {
+  resultBottles = bottlesForDollars(dollars);
+  bottles = resultBottles;
+  var bottleCaps = bottles;
+  while (bottles > 0 || bottleCaps > 0) {
+    bottles = emptyToFull(bottles);
+    bottles += bottleCapsToFull(bottleCaps);
+    bottleCaps = currentBottleCaps.pop(0) + currentBottleCaps.pop(1);
+  }
+  return resultBottles;
+}
+
 
 module.exports = totalBottles;
--- test/bottles-test.js
+++ test/bottles-test.js
@@ -2,19 +2,19 @@ var assert = require("chai").assert;
 var check = require("../index.js");
 
 describe("Total Bottles", function() {
-  it("Calculates how many bottles you can get with your investment", function() {
+  it("Calculates how many bottles you can get if your initial investment is 10", function() {
     var dollars = 10;
     assert.strictEqual(check(dollars), 15);
   })
-  it("Calculates how many bottles you can get with your investment", function() {
+  it("Calculates how many bottles you can get if your initial investment is 20", function() {
     var dollars = 20;
     assert.strictEqual(check(dollars), 35);
   })
-  it("Calculates how many bottles you can get with your investment", function() {
+  it("Calculates how many bottles you can get if your initial investment is 30", function() {
     var dollars = 30;
     assert.strictEqual(check(dollars), 55);
   })
-  it("Calculates how many bottles you can get with your investment", function() {
+  it("Calculates how many bottles you can get if your initial investment is 40", function() {
     var dollars = 40;
     assert.strictEqual(check(dollars), 75);
   })
