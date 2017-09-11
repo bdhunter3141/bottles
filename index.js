c568653cd814ab849c48ee49f0f36a9f1276a81b
Bryan Hunter <bdhunter3141@gmail.com>
2017-09-11 00:01:27 +0000

    Finished working product

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ var dollarAmount = Number(process.argv[2]);
 var resultBottles = null;
 var bottles = null;
 var currentBottleCaps = [];
+var bottlesEarned = null;
+var bottleCapsEarned = null;
+var remainingCaps = [];
+var remainingBottles = [];
 
 var bottlesForDollars = function(dollars) {
-  bottles = Math.floor(dollars / 2);
-  return bottles;
+  var newBottles = Math.floor(dollars / 2);
+  return newBottles;
 }
 
 var emptyToFull = function(bottles) {
   if (bottles > 1) {
     var newBottles = Math.floor(bottles / 2);
+    bottlesEarned += newBottles;
     var leftoverBottles = bottles % 2;
     currentBottleCaps.push(newBottles);
     resultBottles += newBottles;
@@ -24,6 +29,7 @@ var emptyToFull = function(bottles) {
 var bottleCapsToFull = function(bottleCaps) {
   if (bottleCaps > 1) {
     var newBottles = Math.floor(bottleCaps / 4);
+    bottleCapsEarned += newBottles;
     var leftoverBottleCaps = bottleCaps % 4;
     currentBottleCaps.push(leftoverBottleCaps + newBottles);
     resultBottles += newBottles;
@@ -40,9 +46,19 @@ var totalBottles = function(dollars) {
   while (bottles > 0 || bottleCaps > 0) {
     bottles = emptyToFull(bottles);
     bottles += bottleCapsToFull(bottleCaps);
+    remainingBottles.push(bottles);
     bottleCaps = currentBottleCaps.pop(0) + currentBottleCaps.pop(1);
+    remainingCaps.push(bottleCaps);
   }
-  console.log(resultBottles);
+  remainingCaps.pop();
+  remainingBottles.pop();
+  console.log("TOTAL BOTTLES: " + resultBottles);
+  console.log("REMAINING BOTTLES: " + remainingBottles.pop());
+  console.log("REMAINING CAPS: " + remainingCaps.pop())
+  console.log("TOTAL EARNED: ");
+  console.log("   BOTTLES: " + bottlesEarned);
+  console.log("   CAPS: " + bottleCapsEarned);
+  return resultBottles;
 }
 
 totalBottles(dollarAmount);
